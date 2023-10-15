70b4d53de5ac34f9c25083c5454d1770d4db1343
kkomin <100822055+kkomin@users.noreply.github.com>
2023-10-15 18:09:16 +0900

    [LITTLE_FIX] : 🔧 SignIn & SignUp Fragment에서 Actvity로 변경

---
+++ app/src/main/java/com/example/sansaninfo/Activity/SignInActivity.kt
@@ -0,0 +1,12 @@
+package com.example.sansaninfo.Activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.sansaninfo.R
+
+class SignInActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_in)
+    }
+}

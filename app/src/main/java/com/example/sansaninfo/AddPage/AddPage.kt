5f930a0a7ea9655b67a86a1789f8f39e96a4a8c6
Martin Kwon <mindori9097@gmail.com>
2023-10-17 15:00:19 +0900

    [FEAT] : ⚙️: addPage에 backbutton추가

--- app/src/main/java/com/example/sansaninfo/AddPage/AddPage.kt
+++ app/src/main/java/com/example/sansaninfo/AddPage/AddPage.kt
@@ -1,17 +1,24 @@
 package com.example.sansaninfo.AddPage
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.sansaninfo.Main.MainActivity
 import com.example.sansaninfo.R
+import com.example.sansaninfo.databinding.ActivityAddPageBinding
 import com.example.sansaninfo.databinding.ActivityMainBinding
 
 class AddPage : AppCompatActivity() {
 
-    private val binding by lazy{ ActivityMainBinding.inflate(layoutInflater)}
+    private val binding by lazy { ActivityAddPageBinding.inflate(layoutInflater) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
-
+        //뒤로가기
+        binding.addPageIvBackbutton.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }

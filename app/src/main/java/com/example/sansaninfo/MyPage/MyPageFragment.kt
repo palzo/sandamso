5915545fcc2103e66de463bfd7ad79778c0ed10e
Martin Kwon <mindori9097@gmail.com>
2023-10-18 20:50:39 +0900

    [FEAT] : ⚙️: 중간 푸시

--- app/src/main/java/com/example/sansaninfo/MyPage/MyPageFragment.kt
+++ app/src/main/java/com/example/sansaninfo/MyPage/MyPageFragment.kt
@@ -2,19 +2,36 @@ package com.example.sansaninfo.MyPage
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import com.example.sansaninfo.Data.UserData
 import com.example.sansaninfo.SignPage.SignInActivity
 import com.example.sansaninfo.databinding.FragmentMyPageBinding
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.UserProfileChangeRequest
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
+
 
 class MyPageFragment : Fragment() {
 
     private lateinit var binding: FragmentMyPageBinding
+    private var _binding: FragmentMyPageBinding? = null
     private lateinit var mAuth: FirebaseAuth
+    val userData: ArrayList<UserData> = ArrayList()
+    private val mDatabase = FirebaseDatabase.getInstance().getReference("users")
+    val database = Firebase.database
+    val myRef = mDatabase.key
+//    val myRef = database.getReference("${uid}")
+    var changeNickname = ""
 
     companion object {
         fun newInstance() = MyPageFragment()
@@ -27,7 +44,7 @@ class MyPageFragment : Fragment() {
 
         // Inflate the layout for this fragment
         binding = FragmentMyPageBinding.inflate(inflater, container, false)
-
+        binding.myPageEtNickname
         //FirebaseAuth 연결
         mAuth = FirebaseAuth.getInstance()
 
@@ -47,9 +64,38 @@ class MyPageFragment : Fragment() {
             startActivity(intent)
         }
 
+        //수정 버튼
+        binding.myPageIvNickname.setOnClickListener {
+//            val nicknameText = myPage
+//            readNick()
+//            writeNick()
+        }
+        binding.myPageEtNickname.setText("${myRef}")
 
         return binding.root
     }
+
+//    private fun readNick() {
+//        //읽기
+//        myRef.addValueEventListener(object : ValueEventListener {
+//            override fun onDataChange(snapshot: DataSnapshot) {
+//                val test = snapshot.child("nickname")
+//                for (ds in test.children)
+//                    Log.d("martin", ds.toString())
+//            }
+//
+//            //읽기 예외처리
+//            override fun onCancelled(error: DatabaseError) {
+//                Log.d("martin", "Failed", error.toException())
+//            }
+//        })
+//    }
+//
+//    //쓰기
+//    private fun writeNick() {
+//        changeNickname = myRef.setValue("Hello, World").toString()
+//
+//    }
 
     //로그아웃
     private fun signOut() {
@@ -64,10 +110,22 @@ class MyPageFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+//        val profileUpdate = UserProfileChangeRequest.Builder()
+//            .setDisplayName("${nickNameText.text}")
+//            .build()
+//
+//        user?.updateProfile(profileUpdate)
+//            ?.addOnCompleteListener { task ->
+//                if (task.isSuccessful) {
+//                    Toast.makeText(requireContext(), "닉네임을 지정했습니다.", Toast.LENGTH_SHORT).show()
+//                    user?.reload()
+//                }
+//            }
     }
 
-//    override fun onDestroy() {
-//        super.onDestroy()
-//        binding = null
-//    }
+    override fun onDestroyView() {
+        _binding = null
+        super.onDestroyView()
+    }
 }

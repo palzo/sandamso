0756d5286c36621e095964522e4c19c406186fbb
Martin Kwon <mindori9097@gmail.com>
2023-10-18 09:20:48 +0900

    [FIX] : 🐛: 토스트메시지가 뜨지않았던 점 수정, 간헐적으로 로그아웃과 회원탈퇴가 작동하지않던 점 개선

--- app/src/main/java/com/example/sansaninfo/MyPage/MyPageFragment.kt
+++ app/src/main/java/com/example/sansaninfo/MyPage/MyPageFragment.kt
@@ -1,6 +1,5 @@
 package com.example.sansaninfo.MyPage
 
-import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -15,7 +14,6 @@ import com.google.firebase.auth.FirebaseAuth
 class MyPageFragment : Fragment() {
 
     private lateinit var binding: FragmentMyPageBinding
-    private lateinit var mContext: Context
     private lateinit var mAuth: FirebaseAuth
 
     companion object {
@@ -26,6 +24,7 @@ class MyPageFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
         // Inflate the layout for this fragment
         binding = FragmentMyPageBinding.inflate(inflater, container, false)
 
@@ -35,7 +34,7 @@ class MyPageFragment : Fragment() {
         //로그인 페이지로 이동과 로그아웃
         binding.myPageTvLogout.setOnClickListener {
             signOut()
-            Toast.makeText(mContext, "로그아웃이 완료되었습니다.", Toast.LENGTH_SHORT).show()
+            Toast.makeText(activity, "로그아웃이 완료되었습니다.", Toast.LENGTH_SHORT).show()
             val intent = Intent(activity, SignInActivity::class.java)
             startActivity(intent)
         }
@@ -43,48 +42,25 @@ class MyPageFragment : Fragment() {
         //로그인페이지로 이동과 회원탈퇴
         binding.myPageTvSecession.setOnClickListener {
             revokeAccess()
-            Toast.makeText(mContext, "회원탈퇴가 완료되었습니다.", Toast.LENGTH_SHORT).show()
+            Toast.makeText(activity, "회원탈퇴가 완료되었습니다.", Toast.LENGTH_SHORT).show()
             val intent = Intent(activity, SignInActivity::class.java)
             startActivity(intent)
         }
 
 
-//        binding.myPageTvLogout.setOnClickListener {
-//            val intent = Intent(mContext, ActivitySignInBinding::class.java)
-//            startActivity(intent)
-////            signOut()
-//            Toast.makeText(mContext, "로그아웃이 완료되었습니다.", Toast.LENGTH_SHORT).show()
-//        }
-//        binding.myPageTvSecession.setOnClickListener {
-//            revokeAccess()
-//            Toast.makeText(mContext, "회원탈퇴가 완료되었습니다.", Toast.LENGTH_SHORT).show()
-//        }
         return binding.root
     }
 
     //로그아웃
     private fun signOut() {
         FirebaseAuth.getInstance().signOut()
-
     }
 
     //회원탈퇴
     private fun revokeAccess() {
         mAuth.currentUser?.delete()
-
     }
-//    private fun deleted(){
-//        FirebaseAuth.getInstance().currentUser!!.delete().addOnCompleteListener { task ->
-//            if (task.isSuccessful){
-//                Toast.makeText(mContext, "회원 탈퇴가 완료되었습니다.", Toast.LENGTH_SHORT).show()
-//
-//                //로그아웃처리
-//                FirebaseAuth.getInstance().signOut()
-//            }else{
-//                Toast.makeText(mContext, task.exception.toString(),Toast.LENGTH_SHORT).show()
-//            }
-//        }
-//    }
+
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)

109c2dd5627c4f4908acf6a25ce766bc49a854a2
Lim-Juri <imjuly05@gmail.com>
2023-10-19 17:43:12 +0900

    [FEAT] : ⚙️: 갤러리에서 불러온 이미지 스토리지에 저장하기, Add Page에 입력한 데이터 Detail Page로 넘겨주기

--- app/src/main/java/com/example/sansaninfo/AddPage/AddPage.kt
+++ app/src/main/java/com/example/sansaninfo/AddPage/AddPage.kt
@@ -8,19 +8,29 @@ import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import android.Manifest
 import android.content.Intent
+import android.provider.ContactsContract.CommonDataKinds.Nickname
+import android.provider.ContactsContract.DisplayPhoto
+import android.widget.ImageView
+import androidx.core.view.isVisible
 import com.bumptech.glide.Glide
 import com.example.sansaninfo.Data.FBRef
 import com.example.sansaninfo.DetailPage.DetailPage
 import com.example.sansaninfo.Main.MainActivity
+import com.example.sansaninfo.R
 import com.example.sansaninfo.databinding.ActivityAddPageBinding
+import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 import com.google.firebase.storage.ktx.storage
+import java.io.FileOutputStream
+import java.io.IOException
 
 class AddPage : AppCompatActivity() {
 
     private val binding by lazy { ActivityAddPageBinding.inflate(layoutInflater) }
 
     val storage = Firebase.storage("gs://sansaninfo-7819a.appspot.com")
+
+//        private lateinit var imageuri: Uri
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -36,19 +46,21 @@ class AddPage : AppCompatActivity() {
             button.setOnClickListener {
                 val title = addPageTvTitle.text.toString()
                 val maintext = textView7.text.toString()
-//                val time = getData()
                 val user = User(title, maintext)
                 addItem(user)
 
-                val intent = Intent(this@AddPage, DetailPage::class.java)
-                startActivity(intent)
+//                uploadImage(imageuri)
 
+                val intent = Intent(this@AddPage, DetailPage::class.java)
+                intent.putExtra("dataFromAddPageTitle", title)
+                intent.putExtra("dataFromAddPageMaintext", maintext)
+//                intent.putExtra("dataFromAddPageImage",imageuri)
+                startActivity(intent)
             }
         }
 
         binding.addPageIvAdd.setOnClickListener {
             permissionLauncher.launch(Manifest.permission.READ_EXTERNAL_STORAGE)
-            viewImage()
         }
 
         //뒤로가기
@@ -58,39 +70,27 @@ class AddPage : AppCompatActivity() {
         }
     }
 
-    //권한 요청하기
-    val permissionLauncher =
-        registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
-            if (isGranted) {
-                galleryLauncher.launch("image/*")
-            } else {
-                Toast.makeText(baseContext, "외부 저장소 읽기 권한을 승인해야 사용할 수 있습니다.", Toast.LENGTH_LONG)
-                    .show()
+        //권한 요청하기
+        val permissionLauncher =
+            registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
+                if (isGranted) {
+                    galleryLauncher.launch("image/*")
+                } else {
+                    Toast.makeText(baseContext, "외부 저장소 읽기 권한을 승인해야 사용할 수 있습니다.", Toast.LENGTH_LONG)
+                        .show()
+                }
             }
-        }
 
-    //이미지 갤러리 불러오기
-    val galleryLauncher =
-        registerForActivityResult(ActivityResultContracts.GetContent()) { uri ->
-            if (uri != null) {
-                uploadImage(uri)
+        //이미지 갤러리 불러오기
+        val galleryLauncher =
+            registerForActivityResult(ActivityResultContracts.GetContent()) { uri ->
+                if (uri != null) {
+                    uploadImage(uri)
+                }
+                binding.addPageIvAddPhoto.isVisible = true
+                binding.addPageIvAddPhoto.setImageURI(uri)
             }
-        }
 
-    fun viewImage() {
-        // 스토리지 레퍼런스 연결하고 이미지 URI 가져오기
-        val storageReference = storage.reference
-        val pathReference = storageReference.child("images/")
-//        특정 이미지의 경로를 정확하게 지정하기?????????
-
-        pathReference.downloadUrl.addOnSuccessListener { uri ->
-            Glide.with(this).load(uri).into(binding.addPageIvAddPhoto)
-        }.addOnFailureListener{
-            Log.e("스토리지", "이미지 가져오기 에러 => ${it.message}")
-        }
-    }
-
-    //이미지 업로드 하기
     fun uploadImage(uri: Uri) {
         val fullPath = makeFilePath("images", "temp", uri)
         val imageRef = storage.getReference(fullPath)

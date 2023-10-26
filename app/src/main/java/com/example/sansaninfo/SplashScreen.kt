5050a8bd3be39b2972309a1d51ca639be46cc8d9
Lim-Juri <imjuly05@gmail.com>
2023-10-26 16:23:18 +0900

    [FEAT] : ⚙️: Splash 화면 fade_out 효과 넣기

--- app/src/main/java/com/example/sansaninfo/SplashScreen.kt
+++ app/src/main/java/com/example/sansaninfo/SplashScreen.kt
@@ -5,7 +5,6 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
-import com.example.sansaninfo.Main.MainActivity
 import com.example.sansaninfo.SignPage.SignInActivity
 import com.example.sansaninfo.databinding.ActivitySplashScreenBinding
 
@@ -20,10 +19,9 @@ class SplashScreen : AppCompatActivity() {
         Handler(Looper.getMainLooper()).postDelayed({
             val intent = Intent(this, SignInActivity::class.java)
             startActivity(intent)
-            overridePendingTransition(R.anim.slide_down_enter, R.anim.slide_down_exit)
+            overridePendingTransition(R.anim.fade_in,R.anim.fade_out)
             finish()
-        }, 3000)
-
+        }, 0)
 
     }
 }

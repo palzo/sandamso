c53cb4aca9d7da8a36e624c34e66d8ed85c02dc4
kkomin <100822055+kkomin@users.noreply.github.com>
2023-10-30 21:12:14 +0900

    [FIX] : ⚙️ 날씨 API 기능 수정 전 검토용

--- app/src/main/java/com/example/sansaninfo/InfoPage/InfoPage.kt
+++ app/src/main/java/com/example/sansaninfo/InfoPage/InfoPage.kt
@@ -1,6 +1,7 @@
 package com.example.sansaninfo.InfoPage
 
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.location.Geocoder
@@ -44,6 +45,8 @@ import kotlinx.coroutines.launch
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
+import java.text.SimpleDateFormat
+import java.util.Date
 
 class InfoPage : AppCompatActivity(), OnMapReadyCallback {
 
@@ -61,6 +64,12 @@ class InfoPage : AppCompatActivity(), OnMapReadyCallback {
     private val infoPageAdapter by lazy {
         InfoPageAdapter()
     }
+
+    // 현재 날씨 상수
+    val currentDate = Date()
+    @SuppressLint("SimpleDateFormat")
+    val dateFormat = SimpleDateFormat("yyyyMMdd")
+    val today = dateFormat.format(currentDate)
 
     //private val handler = Handler(Looper.getMainLooper())
     private val itemList: List<Item> = mutableListOf()
@@ -149,47 +158,68 @@ class InfoPage : AppCompatActivity(), OnMapReadyCallback {
         /**
         * 아이템 추가 참고용
          */
-//        val testList = mutableListOf<WeatherData>()
-//        for(i in 0..6){
-//            testList.add(
-//                WeatherData(
-//                baseTime = "base: $i",
-//                tmp = "11"
-//            )
-//            )
-//        }
-//        infoPageAdapter.addItem(testList)
+/*        val testList = mutableListOf<WeatherData>()
+        for(i in 0..6){
+            testList.add(
+                WeatherData(
+                baseTime = "base: $i",
+                tmp = "11"
+            )
+            )
+        }
+        infoPageAdapter.addItem(testList)*/
         /**
          * 날씨 API
          */
-        CoroutineScope(Dispatchers.IO).launch {
-            WeatherClient.weatherNetwork.getWeatherInfo(
-                serviceKey = BuildConfig.WEATHER_API_KEY,
-                pageNo = 1,
-                numOfRows = 10,
-                dataType = "JSON",
-                baseDate = 20231026,
-                baseTime = "0500",
-                nx = 21,
-                ny = 132,
-                ).enqueue(object: Callback<Weather?>{
-                override fun onResponse(call: Call<Weather?>, response: Response<Weather?>) {
-                    response.body().let {
-                        it?.response?.body?.items?.item?.forEach {item ->
-//                            weatherDataList.add(WeatherData(
-//
-//                            ))
-                            Log.d("text", "baseDate : ${item.baseDate}, baseTime : ${item.baseTime}, category : ${item.category}")
-                            Log.d("text", "fxstDate : ${item.fcstTime}, fxstDate : ${item.fcstDate}, fxstValue : ${item.fcstValue}")
-                            Log.d("text", "nx : ${item.nx}, ny : ${item.ny}")
+        val baseTimes = listOf("0200", "0500", "0800", "1100", "1400", "1700", "2000", "2300")
+
+        for(baseTime in baseTimes) {
+            CoroutineScope(Dispatchers.IO).launch {
+                WeatherClient.weatherNetwork.getWeatherInfo(
+                    serviceKey = BuildConfig.WEATHER_API_KEY,
+                    pageNo = 1,
+                    numOfRows = 10,
+                    dataType = "JSON",
+                    baseDate = today.toInt(),
+                    baseTime = baseTime,
+                    nx = 21,
+                    ny = 132,
+                ).enqueue(object : Callback<Weather?> {
+                    override fun onResponse(call: Call<Weather?>, response: Response<Weather?>) {
+                        response.body().let {
+                            it?.response?.body?.items?.item?.forEach { item ->
+                                /*if(item.category == "TMX") {
+                                    val tmpValue = item.fcstValue
+                                    weatherDataList.add(WeatherData(baseTime, tmpValue))
+                                }*/
+                                if (item.category == "TMP") {
+                                    weatherDataList.add(
+                                        WeatherData(
+                                            baseTime = "${item.fcstValue}",
+                                            tmp = "11"
+                                        )
+                                    )
+                                }
+                                Log.d(
+                                    "text",
+                                    "baseDate : ${item.baseDate}, baseTime : ${item.baseTime}, category : ${item.category}"
+                                )
+                                Log.d(
+                                    "text",
+                                    "fxstDate : ${item.fcstTime}, fxstDate : ${item.fcstDate}, fxstValue : ${item.fcstValue}"
+                                )
+                                Log.d("text", "nx : ${item.nx}, ny : ${item.ny}")
+                            }
+                        }
+                        runOnUiThread {
+                            infoPageAdapter.addItem(weatherDataList)
                         }
                     }
-
-                }
-                override fun onFailure(call: Call<Weather?>, t: Throwable) {
-                    Log.e("error", "${t.message}")
-                }
-            })
+                    override fun onFailure(call: Call<Weather?>, t: Throwable) {
+                        Log.e("error", "${t.message}")
+                    }
+                })
+            }
         }
     }
     private fun removeSpecialCharacters(inputText: String): String {

69dc5cad7fc32baf94f5279abbd59fa59ba21560
Lim-Juri <imjuly05@gmail.com>
2023-11-03 22:04:46 +0900

    [FEAT] : ⚙️: 채팅방 구현하기 대실패

--- app/src/main/java/com/example/sansaninfo/Chatting/ChattingListFragment.kt
+++ app/src/main/java/com/example/sansaninfo/Chatting/ChattingListFragment.kt
@@ -1,14 +1,23 @@
 package com.example.sansaninfo.Chatting
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.sansaninfo.R
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.sansaninfo.Data.FBRoom
+import com.example.sansaninfo.Data.RoomData
 import com.example.sansaninfo.databinding.FragmentChattingListBinding
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
+import java.util.zip.Inflater
 
 class ChattingListFragment : Fragment() {
+
+    val roomList = mutableListOf<RoomData>()
 
     private val chattingListAdapter by lazy {
         ChattingListAdapter()
@@ -18,6 +27,9 @@ class ChattingListFragment : Fragment() {
     private val binding get() = _binding!!
 
     companion object {
+
+        var userId: String = ""
+        var userName: String = ""
         fun newInstance() = ChattingListFragment()
     }
 
@@ -25,14 +37,42 @@ class ChattingListFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        _binding = FragmentChattingListBinding.inflate(inflater, container, false)
 
-        return binding.root
+        _binding = FragmentChattingListBinding.inflate(inflater, container, false)
+        val view = binding.root
+
+        binding.recyclerViewChattingRoom.layoutManager = LinearLayoutManager(requireContext())
+        binding.recyclerViewChattingRoom.adapter = chattingListAdapter
+
+        return view
+    }
+
+    override fun onResume() {
+        super.onResume()
+        //데이터베이스에서 데이터 읽어오기
+        loadRooms()
     }
 
     override fun onDestroy() {
         super.onDestroy()
         _binding = null
+    }
+
+    // 채팅방 목록 만들기
+    private fun loadRooms() {
+        FBRoom.roomRef.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                Log.d("load Rooms", "Data received")
+                roomList.clear()
+                for (item in snapshot.children) {
+                    item.getValue(RoomData::class.java)?.let { room -> roomList.add(room) }
+                }
+                chattingListAdapter.notifyDataSetChanged()
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                Log.d("load Rooms", "error = $error")
+            }
+        })
     }
 }

7012ccd380c3e6776a05cdb9498659d4bb593c3c
Lim-Juri <imjuly05@gmail.com>
2023-11-09 11:58:43 +0900

    [FIX] : 🔧 RoomData class -> data class로 변경

--- app/src/main/java/com/sandamso/sansaninfo/Data/RoomData.kt
+++ app/src/main/java/com/sandamso/sansaninfo/Data/RoomData.kt
@@ -1,15 +1,8 @@
 package com.sandamso.sansaninfo.Data
 
 
-class RoomData {
-    var id: String = ""
-    var title: String = ""
+data class RoomData (
+    var id: String = "",
+    var title: String = "",
     var users: String = ""
-
-    constructor()
-
-    constructor(title: String, creatorName: String) {
-        this.title = title
-        users = creatorName
-    }
-}
+)

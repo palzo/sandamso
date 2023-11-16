509daf11ac77e2359e3533eddaa0d849b8a6c8d5
Martin Kwon <mindori9097@gmail.com>
2023-11-16 15:35:03 +0900

    [FEAT]: 튜토리얼페이지 소스 추가

---
+++ app/src/main/java/com/sandamso/sansaninfo/TutorialActivity/TutorialActivity.kt
@@ -0,0 +1,12 @@
+package com.sandamso.sansaninfo.TutorialActivity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.sandamso.sansaninfo.R
+
+class TutorialActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tutorial)
+    }
+}

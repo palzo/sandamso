348a79c2585014e0db55ac759c91a05f944e58be
DESKTOP-I0HC5TC\rotqj <rotqjfrkffo@gmail.com>
2023-12-11 16:23:04 +0900

    encryption 1. SharedPreference에 사용자 정보를 저장할 때 인코딩합니다. 2. 사용자 정보를 사용 시 저장 되어있는 데이터를 디코딩합니다.

--- app/src/main/java/com/sandamso/sansaninfo/SplashScreen.kt
+++ app/src/main/java/com/sandamso/sansaninfo/SplashScreen.kt
@@ -1,22 +1,28 @@
 package com.sandamso.sansaninfo
 
 import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.util.Log
+import androidx.annotation.RequiresApi
 import com.sandamso.sansaninfo.main.MainActivity
 import com.sandamso.sansaninfo.signpage.SignInActivity
 import com.sandamso.sansaninfo.databinding.ActivitySplashScreenBinding
 import com.google.firebase.auth.FirebaseAuth
 import com.sandamso.sansaninfo.TutorialActivity.TutorialActivity
 import java.lang.Exception
+import java.util.Base64
+
 
 class SplashScreen : AppCompatActivity() {
 
     private val binding by lazy { ActivitySplashScreenBinding.inflate(layoutInflater) }
     private val auth by lazy { FirebaseAuth.getInstance() }
 
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
@@ -24,18 +30,24 @@ class SplashScreen : AppCompatActivity() {
         val loginInfo = getSharedPreferences("prefLogin", 0)
         val tutorial = loginInfo.getString("tutorial", "")
         val email = loginInfo.getString("email", "")
-        val pw = loginInfo.getString("pw", "")
-        if(tutorial == ""){
+        var decodingPw = loginInfo.getString("pw", "")
+        try {
+            decodingPw = decodingPw?.let { decode(it) }
+
+        } catch (e: Exception) {
+            Log.d("encryption", "${e.message}")
+        }
+        if (tutorial == "") {
             Handler(Looper.getMainLooper()).postDelayed({
                 startTutorialActivity()
             }, 0)
-        }else {
+        } else {
             // 저장된 데이터 불러오기
             if (loginInfo.getString("loginType", "") == "2") {
                 // 자동 로그인 기능
-                if (email != null && pw != null) {
-                    try{
-                        auth.signInWithEmailAndPassword(email, pw)
+                if (email != null && decodingPw != null) {
+                    try {
+                        auth.signInWithEmailAndPassword(email, decodingPw)
                             .addOnCompleteListener {
                                 val user = auth.currentUser
                                 if (user != null && user.isEmailVerified) {
@@ -44,7 +56,7 @@ class SplashScreen : AppCompatActivity() {
                                     }, 0)
                                 }
                             }
-                    }catch (e: Exception){
+                    } catch (e: Exception) {
                         val loginInfo = getSharedPreferences("prefLogin", 0)
                         val loginInfoEdit = loginInfo.edit()
                         loginInfoEdit.putString("loginType", "0")
@@ -61,7 +73,6 @@ class SplashScreen : AppCompatActivity() {
                 }, 0)
             }
         }
-
     }
     private fun startMainActivity() {
         val intent = Intent(this, MainActivity::class.java)
@@ -80,5 +91,11 @@ class SplashScreen : AppCompatActivity() {
         startActivity(intent)
         overridePendingTransition(R.anim.fade_in, R.anim.fade_out)
         finish()
+    }
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun decode(input: String): String {
+        val decodedBytes = Base64.getDecoder().decode(input.toByteArray())
+        Log.d("encryption", "decoding : $decodedBytes")
+        return String(decodedBytes)
     }
 }
